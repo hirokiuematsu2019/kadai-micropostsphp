ad8ce6619f4ed8ba5de00e9572302066993d3dcd
hiroki.uematsu <uep74549@gmail.com>
2019-06-01 06:42:50 +0000

    favorite

--- app/User.php
+++ app/User.php
@@ -42,6 +42,11 @@ class User extends Authenticatable
         return $this->belongsToMany(User::class,'user_follow','follow_id','user_id')->withTimestamps();
     }
     
+    public function favorites()//add
+    {
+        return $this->belongsToMany(Micropost::class, 'favorites', 'user_id', 'micropost_id')->withTimestamps();
+    }
+    
     public function follow($userId)
     {
         //既にフォローしているかの確認
@@ -70,15 +75,49 @@ class User extends Authenticatable
         
     }
     
-    public function feed_microposts()
-    {
-        $follow_user_ids = $this->followings()->pluck('users.id')->toArray();
-        $follow_user_ids[] = $this->id;
-        return Micropost::whereIn('user_id', $follow_user_ids);
-    }
+
     public function is_following($userId)
     {
         return $this->followings()->where('follow_id',$userId)->exists();
         
+    }
+    
+     public function favorite($micropostId)
+    {
+        //既にお気に入りしているかの確認
+        $exist = $this->is_favoriting($micropostId);
+        
+        if($exist){
+            return false;
+        }else{
+            $this->favorites()->attach($micropostId);
+            return true;
+        }
+    }
+    public function unfavorite($micropostId)
+    {
+        //既にお気に入りにしているかの確認
+        $exist = $this->is_favoriting($micropostId);
+
+        
+        if($exist){
+            $this->favorites()->detach($micropostId);
+            return true;
+        } else {
+            return false;
+        }    
+        
+    }
+    
+    public function is_favoriting($micropostId)
+    {
+        return $this->favorites()->where('micropost_id',$micropostId)->exists();
+        
+    }
+        public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()->pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
     }
 }
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -1,4 +1,4 @@
-<ul class="list-unstyled">
+<ul class="media-list">
     @foreach ($microposts as $micropost)
         <li class="media mb-3">
             <img class="mr-2 rounded" src="{{ Gravatar::src($micropost->user->email, 50) }}" alt="">
@@ -8,6 +8,25 @@
                 </div>
                 <div>
                     <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
+                </div>
+                <div class="row">
+   
+                   @if (Auth::user()->is_favoriting($micropost->id))
+                        {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+                        {!! Form::submit('Unfavorite', ['class' => "btn btn-info btn-sm"]) !!}
+                        {!! Form::close() !!}
+                    @else
+                        {!! Form::open(['route' => ['favorites.favorite' , $micropost->id]]) !!}
+                        {!! Form::submit('favorite',['class' => "btn btn-warning btn-sm"]) !!}
+                        {!! Form::close() !!}
+                    @endif
+                    
+                    @if (Auth::id() == $micropost->user_id)
+                        {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+                        {!! Form::close() !!}
+                    @endif
+
                 </div>
             </div>
         </li>
